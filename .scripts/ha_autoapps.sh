1f071267b6b8adab46e0879d194c783f9bf9d02d
David <50021531+Anonymoesje@users.noreply.github.com>
2023-12-06 20:54:22 +0100

    Update ha_autoapps.sh

--- .scripts/ha_autoapps.sh
+++ .scripts/ha_autoapps.sh
@@ -3,39 +3,26 @@
 set -e
 
 ##############################
-# Download packages
+# Automatic apps download #
 ##############################
 
 PACKAGES="$1"
 echo "To install : $PACKAGES"
-echo "Before bash"
+
 # Install bash if needed
 if ! command -v bash >/dev/null 2>/dev/null; then
-    echo "attempt bash"
     (apt-get update && apt-get install -yqq --no-install-recommends bash || apk add --no-cache bash) >/dev/null
 fi
-echo "Before curl"
+
 # Install curl if needed
 if ! command -v curl >/dev/null 2>/dev/null; then
-    echo "attempt curl"
     (apt-get update && apt-get install -yqq --no-install-recommends curl || apk add --no-cache curl) >/dev/null
 fi
-echo "Before wget"
-# Install wget if needed
-if ! command -v wget >/dev/null 2>/dev/null; then
-    echo "attempt wget"
-    (apt-get update && apt-get install -yqq --no-install-recommends wget || apk add --no-cache wget) >/dev/null
-fi
-
-
-echo "Before eval"
 
 # Call apps installer script if needed
 curl -f -L -s -S "https://raw.githubusercontent.com/Anonymoesje/home-assistant-magic/main/.scripts/ha_automatic_packages.sh" --output /ha_automatic_packages.sh
 chmod 777 /ha_automatic_packages.sh
 eval /./ha_automatic_packages.sh "${PACKAGES:-}"
-
-echo "Done evaluating"
 
 # Clean
 rm /ha_automatic_packages.sh
